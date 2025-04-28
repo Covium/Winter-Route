c527ebbff2f0993c3cddb56d3686a8e11bb28e2b
Septimus <SeptimusVega@proton.me>
2025-04-28 17:39:36 +0300

    refactor: renamed skill levels

--- src/features/types/personalSkill.ts
+++ src/features/types/personalSkill.ts
@@ -7,9 +7,9 @@ export class PersonalSkill extends Skill {
   nextSubskill!: string
 
   private static readonly skillLevelMap: { [key: number]: string } = {
-    0: 'Начинающий',
-    1: 'Уверенный',
-    2: 'Профессионал',
+    0: 'Новичок',
+    1: 'Продвинутый',
+    2: 'Эксперт',
   }
   static getSkillLevelName(skillLevel: number): string {
     return this.skillLevelMap[skillLevel] || 'Неизвестный'

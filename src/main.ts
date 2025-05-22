d4a881e4cea78f68eff1c13a4cee493bb23f8ea2
Septimus <SeptimusVega@proton.me>
2025-05-22 14:54:41 +0300

    refactor: renamed GeneralStatistics.vue to HomePage.vue

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '@/App.vue'
-import GeneralStatistics from '@/components/pages/main-page/GeneralStatistics.vue'
+import GeneralStatistics from '@/components/pages/main-page/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
